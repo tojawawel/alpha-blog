3f0578ef6fa78d83e285ca14fec184becc9aca86
tojawawel <bgawel93@gmail.com>
2018-09-26 12:16:00 +0200

    Added delete method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = Article.all 
+    @articles = Article.all
   end
 
   def new
@@ -34,6 +34,13 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Deleted successfully"
+    redirect_to articles_path
   end
 
 private
