b96a02d8abaecbd7ea9492ab69374237aea80d10
tojawawel <bgawel93@gmail.com>
2018-10-02 13:03:36 +0200

    errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_101450) do
+ActiveRecord::Schema.define(version: 2018_09_29_105927) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_101450) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "users"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
